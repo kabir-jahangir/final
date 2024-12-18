41e4eeaf4aba33338ee432ccbcf14120c37e5380
jahangir kabir <kabir.jahangir.sk@gmail.com>
2024-12-18 21:43:29 +0600

    Update Register.jsx

--- Register.jsx
+++ Register.jsx
@@ -1,7 +1,12 @@
 // ==============import part start
 import React from 'react'
 import { FaRegEyeSlash } from "react-icons/fa6";
-import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword ,sendEmailVerification, updateProfile} from "firebase/auth";
+import 'react-toastify/dist/ReactToastify.css';
+import {Bounce,toast} from 'react-toastify'
+import { useState, CSSProperties } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import {  signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 // ==============import part end
 
 const Register = () => {
@@ -11,22 +16,106 @@ const Register = () => {
     const [emailError, setemailError] = useState('');
     const [passwordError, setpasswordError] = useState('');
     const [show, setshow] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const provider = new GoogleAuthProvider();
     // =================variable part end
+    // =================firebase variable part
+    const auth = getAuth();
     // =================function part start
     const handleshow = ()=>{
         setshow(!show)
     }
    const handleSubmit = (e)=>{
+    setLoading(true)
     e.preventDefault()
     if(!email){
         setemailError('enter your email')
     }
     if(!password){
         setpasswordError('enter your password')
+    }else{
+            
+        createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            console.log("User registered:",user)
+            sendEmailVerification(auth.currentUser)
+                .then(() => {
+                    setLoading(false)
+
+
+                    //==========set user name and profile pic
+                    updateProfile(auth.currentUser,{
+                        
+                        displayName: email, 
+                        
+                      }).then(() => {
+                        
+                        toast.success('Email Verification Send!Please Check Your Email And Click On The Link', {
+                            position: "top-center",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "dark",
+                            transition: Bounce,
+                            });
+                      // Email verification sent!
+                      // ...
+                        // Profile updated!
+                        // ...
+                      })                    
+});  
+          })
+          .catch((error) => {
+            setLoading(false)
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log('this is cde',errorCode,'this is msg',errorMessage)
+            if(errorCode == 'auth/email-already-in-use'){
+                toast.error('Email Has Already Taken!!!Please Use Proper Email.Thank You!!!', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    transition: Bounce,
+                    });
+            }
+            // ..
+          });
     }
    }
+       // =========================google sign in button
+       const handleGoogle = ()=>{
+        signInWithPopup(auth, provider)
+             .then((result) => {
+               // This gives you a Google Access Token. You can use it to access the Google API.
+               const credential = GoogleAuthProvider.credentialFromResult(result);
+               const token = credential.accessToken;
+               // The signed-in user info.
+               const user = result.user;
+               // IdP data available using getAdditionalUserInfo(result)
+               // ...
+                        }).catch((error) => {
+               // Handle Errors here.
+               const errorCode = error.code;
+               const errorMessage = error.message;
+                          // The email of the user's account used.
+                          const email = error.customData.email;
+               // The AuthCredential type that was used.
+               const credential = GoogleAuthProvider.credentialFromError(error);
+               // ...
+             });
+            
+               }
    //==================function part end
-
   return (
     <>
         <div className = 'login justify-items-center '>
@@ -56,16 +145,24 @@ const Register = () => {
                         </div>
                         {/* =================sign in button part=================== */}
                         <div className='mb-[36px]'>
-                            <button className='w-[290px] h-[40px]  lg:w-[640px] lg:h-[160px]' onClick={handleSubmit} ><h2 className= 'text-[#FFF] text-[12px] px-[65px] py-[12px] lg:text-[#fff] lg:text-[32px] font-extrabold font-Nunito bg-[#8699DA] lg:px-[265px] lg:py-[24px] rounded-[50px]'>Sign in</h2></button>
+                            {
+                                loading?(
+                                    <button className='w-[290px] h-[40px]  lg:w-[640px] lg:h-[160px]' onClick={handleSubmit}  ><h2 className= 'text-[#FFF] text-[12px] px-[65px] py-[12px] lg:text-[#fff] lg:text-[32px] font-extrabold font-Nunito bg-[#8699DA] lg:px-[265px] lg:py-[24px] rounded-[50px]'><ClipLoader hover:color = 'fff' /></h2></button>
+
+                                )
+                                :
+                                ( <button className='w-[290px] h-[40px]  lg:w-[640px] lg:h-[160px]' onClick={handleSubmit}><h2 className= 'text-[#FFF] text-[12px] px-[65px] py-[12px] lg:text-[#fff] lg:text-[32px] font-extrabold font-Nunito bg-[#8699DA] lg:px-[265px] lg:py-[24px] rounded-[50px]'>Sign in</h2></button>)
+                            }
+                            
                         </div>
                         {/* =================continue with google facebook apple part=================== */}
                         <div className='mb-[16px] place-self-center '>
                             <img src='Images/1.png' className='w-[200px] h-[20px]   lg:w-[480px] lg:h-[43px]' ></img>
                         </div>
                         <div className = 'flex place-self-center gap-[3px] mb-[109px] lg:flex lg:gap-[16px] lg:place-self-center lg:mb-[109px] '>
-                            <img src='Images/google.png' className='w-[80px] h-[50px] lg:w-[150px] lg:h-[100px]'></img>
-                            <img src='Images/facebook.png' className='w-[80px] h-[50px] lg:w-[150px] lg:h-[100px]'></img>
-                            <img src='Images/apple.png' className='w-[80px] h-[50px] lg:w-[150px] lg:h-[100px]'></img>
+                            <button onClick = {handleGoogle}><img src='Images/google.png' className='w-[80px] h-[50px] lg:w-[150px] lg:h-[100px]'></img></button>
+                            <button><img src='Images/facebook.png' className='w-[80px] h-[50px] lg:w-[150px] lg:h-[100px]'></img></button>
+                            <button><img src='Images/apple.png' className='w-[80px] h-[50px] lg:w-[150px] lg:h-[100px]'></img></button>
                             
                         </div>
                     </div>
@@ -80,3 +177,4 @@ const Register = () => {
 )
 }
 export default Register
+
